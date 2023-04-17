f3df9ebeb00252995982de685f623ba7819f87ee
Jimmy Adediran <jim_adediran@yahoo.com>
2023-04-17 19:16:16 -0400

    Added word music, now reads build your music library in the footer

--- MusicLibrary_Frontend1/music_library/src/App.js
+++ MusicLibrary_Frontend1/music_library/src/App.js
@@ -25,7 +25,7 @@ async function getAllSongs(){
       <div className='header'><SearchBar songs={songs} setSongs={setSongs}/></div>
       <div className='header'><DisplaySong className='header' songs={songs}  getAllSongs={getAllSongs}/></div>
       <div className='header'><AddSong getAllSongs={getAllSongs} /></div>
-      <div className='footer'>Build <br></br> Your <br></br>Library<br></br>Here!</div>
+      <div className='footer'>Build <br></br>Your<br></br>Music<br></br>Library<br></br>Here!</div>
     </div>
     
   );
